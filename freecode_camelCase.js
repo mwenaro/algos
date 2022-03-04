7fd723e0cb2f38e1f723f98dd2d8e6042747ef37
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-03-04 15:07:48 +0300

    Create freecode_camelCase.js

---
+++ freecode_camelCase.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+  if(!str) return ;
+  return str.toLowerCase()
+.split(' ')
+.map(word=>word.charAt(0).toUpperCase()+word.slice(1))
+  .join(' ');
+}
+
+titleCase("I'm a little tea pot");
